bc2029febb90fada9a857e1cbf025fc3d78e0746
Santhosh mani <pitakasanthosh@gmail.com>
2025-06-27 08:15:17 +0530

    chore: Update App behaviour showing in recent apps

--- app/src/main/java/com/simplelauncher/HomeFragment.kt
+++ app/src/main/java/com/simplelauncher/HomeFragment.kt
@@ -107,6 +107,8 @@ class HomeFragment : Fragment() {
                         component = ComponentName(app.packageName, app.className)
                         action = Intent.ACTION_MAIN
                         addCategory(Intent.CATEGORY_LAUNCHER)
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or
+                                Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED
                     }
                     startActivity(launchIntent)
                 }

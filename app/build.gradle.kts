5d67200c94b11cfc0365960c5af3e69497d07477
Santhosh mani <pitakasanthosh@gmail.com>
2025-07-11 15:23:26 +0530

    fix: back button weird behaviour on Homescreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,6 @@ dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2025.05.00")
     implementation(composeBom)
     androidTestImplementation(composeBom)
-    implementation("androidx.activity:activity-compose:1.10.1")
     implementation ("androidx.compose.runtime:runtime")
     implementation ("androidx.compose.ui:ui")
     implementation ("androidx.compose.foundation:foundation")

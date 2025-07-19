ed14b53ef03f108dd61da372b1e74474c8ce54c9
Santhosh mani <pitakasanthosh@gmail.com>
2025-07-19 18:10:18 +0530

    fix: Improve UX of exiting zen with confirmation from user

--- app/src/main/java/com/zenlauncher/ui/screens/SettingsActivity.kt
+++ app/src/main/java/com/zenlauncher/ui/screens/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.zenlauncher.ui.screens
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -12,6 +13,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,6 +24,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.zenlauncher.helpers.Constants
 import com.zenlauncher.helpers.SettingsUtils
+import com.zenlauncher.ui.components.settings.ExitZenLauncherDialog
 import com.zenlauncher.ui.components.settings.SettingsHeader
 import com.zenlauncher.ui.components.settings.SettingsOption
 import com.zenlauncher.ui.components.settings.ZenTopAppBar
@@ -37,6 +43,7 @@ class SettingsActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ZenLauncherSettingsScreen(onBackPressed: () -> Unit) {
+    var showExitDialog by remember { mutableStateOf(false) }
     val context = LocalContext.current
     Scaffold(
         topBar = {
@@ -91,8 +98,16 @@ fun ZenLauncherSettingsScreen(onBackPressed: () -> Unit) {
             Spacer(modifier = Modifier.height(Constants.Settings.SPACING_LARGE.dp))
 
             SettingsOption("Exit Zen Launcher") {
-                (context as? ComponentActivity)?.finishAffinity()
+                showExitDialog = true
             }
+        }
+
+        if (showExitDialog) {
+            Log.d("Exiting", "Here we go")
+            ExitZenLauncherDialog(
+                context = context,
+                onDismiss = { showExitDialog = false }
+            )
         }
     }
 }
